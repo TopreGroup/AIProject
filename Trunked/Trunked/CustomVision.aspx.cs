93a5856051395288929053c720541803bb9037d0
DanielMidler <d.midler@hotmail.com>
2018-09-12 15:13:51 +1000

    Added new solution with core functionality for the non-prototype Trunked solution

---
+++ Trunked/Trunked/CustomVision.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training; // -Version 0.12.0-preview
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction; // -Version 0.10.0-preview
+
+namespace TrunkedPrototypes
+{
+    public partial class CustomVision : Page
+    {
+        protected string trainingKey = ConfigurationManager.AppSettings["CustomVisionTrainingKey"];
+        protected string predictionKey = ConfigurationManager.AppSettings["CustomVisionPredictionKey"];
+        protected Guid projectID = new Guid(ConfigurationManager.AppSettings["CustomVisionProjectID"]);
+
+        protected TrainingApi trainingApi;
+
+        protected int minImagesPerTag = 5;
+
+        protected string recognizedText = "";
+        protected bool resultsFound;
+        protected List<Dictionary<string, string>> bookDetailsList;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", Server.MapPath("~/Content/") + "VisionAPIServiceAccount.json");
+
+            trainingApi = new TrainingApi() { ApiKey = trainingKey };
+            trainingApi.HttpClient.Timeout = new TimeSpan(0, 30, 0);
+        }
+
+        protected void RecognizeButton_Click(object sender, EventArgs e)
+        {
+            if (ctrlFileUpload.HasFile)
+            {
+                string path = "";
+
+                try
+                {
+                    string fileName = Path.GetFileName(ctrlFileUpload.FileName);
+
+                    path = Server.MapPath("~/UploadedImages/") + fileName;
+
+                    ctrlFileUpload.SaveAs(path);
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                    lblStatus.Visible = true;
+                }
+
+                try
+                {
+                    MakePrediction(path);
+                }
+                catch (Microsoft.Rest.HttpOperationException ex)
+                {
+                    lblResults.Text = "ERROR: " + ex.Message + "<br />" + ex.Response.Content;
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = ex + "<br />" + ex.Message + "<br />" + ex.InnerException;
+                }
+            }
+
+            ctrlFileUpload.Dispose();
+        }
+       
+        protected void MakePrediction(string predictionImagePath)
+        {
+            PredictionEndpoint endpoint = new PredictionEndpoint() { ApiKey = predictionKey };
+            MemoryStream predictionImage = new MemoryStream(File.ReadAllBytes(predictionImagePath));
+
+            CreateResultsTable(endpoint.PredictImage(projectID, predictionImage));
+        }
+
+        protected void CreateResultsTable(Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models.ImagePrediction result)
+        {
+            lblResults.Visible = false;
+
+            List<string> headings = new List<string>()
+            {
+                "Tag", "Probability"
+            };
+
+            TableRow row = new TableRow();
+            TableCell cell;
+
+            foreach (string heading in headings)
+            {
+                cell = new TableCell();
+
+                cell.Controls.Add(new LiteralControl(heading));
+                cell.CssClass = "tblCell heading";
+
+                row.Cells.Add(cell);
+            }
+
+            tblResults.Rows.Add(row);
+
+            foreach (var prediction in result.Predictions)
+            {
+                row = new TableRow();
+
+                for (int i = 1; i <= headings.Count; i++)
+                {
+                    cell = new TableCell();
+
+                    string cellValue = "";
+
+                    if (i == 1)
+                        cellValue = prediction.TagName;
+                    else if (i == 2)
+                        cellValue = prediction.Probability.ToString();
+
+                    cell.Controls.Add(new LiteralControl(cellValue));
+                    cell.CssClass = "tblCell";
+
+                    row.Cells.Add(cell);
+                }
+
+                tblResults.Rows.Add(row);
+                resultsFound = true;
+            }
+
+            if (resultsFound)
+                tblResults.Visible = true;
+        }
+    }
+}
