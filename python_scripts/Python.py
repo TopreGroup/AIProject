58d6e62887b397bd3f7831fd40c82c5e23e953cf
Ryan <s3419529@student.rmit.edu.au>
2018-09-20 11:50:04 +1000

    python script added

---
+++ python_scripts/Python.py
@@ -0,0 +1,79 @@
+
+# coding: utf-8
+
+# ###  This is a test
+# <p style= "color: green"> Title </p>
+# 
+
+# In[5]:
+
+
+
+from azure.cognitiveservices.vision.customvision.training import training_api
+from azure.cognitiveservices.vision.customvision.training.models import ImageUrlCreateEntry
+
+# Replace with a valid key
+training_key = "1bb97a85cb31486ea08500867b4e5e7a"
+prediction_key = "ccc9e16826214050ac955205234b63ac"
+
+trainer = training_api.TrainingApi(training_key)
+
+# Create a new project
+print ("Creating project...")
+project = trainer.create_project("Test")
+
+
+# In[6]:
+
+
+# Make two tags in the new project
+hemlock_tag = trainer.create_tag(project.id, "Hemlock")
+cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
+
+
+# In[7]:
+
+
+base_image_url = "https://raw.githubusercontent.com/Microsoft/Cognitive-CustomVision-Windows/master/Samples/"
+
+print ("Adding images...")
+for image_num in range(1,10):
+    image_url = base_image_url + "Images/Hemlock/hemlock_{}.jpg".format(image_num)
+    trainer.create_images_from_urls(project.id, [ ImageUrlCreateEntry(url=image_url, tag_ids=[ hemlock_tag.id ] ) ])
+
+for image_num in range(1,10):
+    image_url = base_image_url + "Images/Japanese Cherry/japanese_cherry_{}.jpg".format(image_num)
+    trainer.create_images_from_urls(project.id, [ ImageUrlCreateEntry(url=image_url, tag_ids=[ cherry_tag.id ] ) ])
+
+
+# Alternatively, if the images were on disk in a folder called Images alongside the sample.py, then
+# they can be added by using the following:
+#
+#import os
+#hemlock_dir = "Images\\Hemlock"
+#for image in os.listdir(os.fsencode("Images\\Hemlock")):
+#    with open(hemlock_dir + "\\" + os.fsdecode(image), mode="rb") as img_data: 
+#        trainer.create_images_from_data(project.id, img_data, [ hemlock_tag.id ])
+#
+#cherry_dir = "Images\\Japanese Cherry"
+#for image in os.listdir(os.fsencode("Images\\Japanese Cherry")):
+#    with open(cherry_dir + "\\" + os.fsdecode(image), mode="rb") as img_data: 
+#        trainer.create_images_from_data(project.id, img_data, [ cherry_tag.id ])
+
+
+# In[8]:
+
+
+import time
+
+print ("Training...")
+iteration = trainer.train_project(project.id)
+while (iteration.status != "Completed"):
+    iteration = trainer.get_iteration(project.id, iteration.id)
+    print ("Training status: " + iteration.status)
+    time.sleep(1)
+
+# The iteration is now trained. Make it the default project endpoint
+trainer.update_iteration(project.id, iteration.id, is_default=True)
+print ("Done!")
+
