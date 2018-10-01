82322d9fe86356bd5c29608112d370df1009db88
Rita <rita.h.tse@gmail.com>
2018-10-01 17:51:56 +1000

    updated syntax on sprocs

--- sprocs/insert_book.sql
+++ sprocs/insert_book.sql
@@ -3,10 +3,11 @@ GO
 
 IF EXISTS (
     SELECT 1 
-    FROM sys.procedures)
-    WHERE Name = 'usp_book_insert'
+    FROM sys.objects
+    WHERE object_id = OBJECT_ID(N'[dbo].[usp_book_insert]')
+    AND type  in (N'P', N'PC')
     )
-    DROP PROCEDURE dbo.usp_book_insert
+    DROP PROCEDURE [dbo].[usp_book_insert]
 GO
 
 
